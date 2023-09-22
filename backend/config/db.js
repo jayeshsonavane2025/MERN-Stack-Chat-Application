aa19b1772f5f04858b1f18bbe5215a616dce23f8
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-22 15:38:08 +0530

    update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,16 +4,25 @@ const colors = require("colors");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
+<<<<<<< Updated upstream
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
+=======
+        useNewUrlParser: true ,
+      useUnifiedTopology: true
+>>>>>>> Stashed changes
      
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
+<<<<<<< Updated upstream
   
     console.error(Error: ${error.message}.red.bold);
+=======
+    console.log(`Error: ${error.message}`.red.bold);
+>>>>>>> Stashed changes
     process.exit();
   }
 };
