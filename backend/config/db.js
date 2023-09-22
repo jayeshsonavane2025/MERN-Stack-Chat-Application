944ba9e52a9c1c01d590ea53058bc7107a7606d2
jayeshsonavane2025 <jayeshsonavane2025@gmail.com>
2023-09-22 21:43:50 +0530

    jwt authentication & bcrypt done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,19 @@
+require('dotenv').config();
+
 const mongoose = require("mongoose");
 const colors = require("colors");
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
+  useNewUrlParser: true,
       useUnifiedTopology: true,
-     useCreateIndex: true
+  
+  
     });
+    console.log("Connected successfully".yellow.bold);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.yellow.bold );
   } catch (error) {
     console.log(`Error: ${error.message}`.red.bold);
     process.exit();
